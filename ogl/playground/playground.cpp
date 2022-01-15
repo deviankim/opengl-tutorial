18b9f1787f815da7838aac3b7a73ee5b2eebd4b5
kimss <developer.kss@gmail.com>
2022-01-15 21:52:29 +0900

    failed

--- ogl/playground/playground.cpp
+++ ogl/playground/playground.cpp
@@ -60,9 +60,50 @@ int main( void )
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
+    
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+    
+    // 3 버텍스들을 표현하는 3 벡터들의 배열
+    static const GLfloat g_vertex_buffer_data[] = {
+       -1.0f, -1.0f, 0.0f,
+       1.0f, -1.0f, 0.0f,
+       0.0f,  1.0f, 0.0f,
+    };
+    
+    // 이것이 우리의 버텍스 버퍼를 가리킵니다.
+    GLuint vertexbuffer;
+    // 버퍼를 하나 생성합니다. vertexbuffer 에 결과 식별자를 넣습니다
+    glGenBuffers(1, &vertexbuffer);
+    // 아래의 명령어들은 우리의 "vertexbuffer" 버퍼에 대해서 다룰겁니다
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // 우리의 버텍스들을 OpenGL로 넘겨줍니다
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    
 
 	do{
-		// Draw nothing, see you in tutorial 2 !
+		
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Use our shader
+        glUseProgram(programID);
+
+        // 버퍼의 첫번째 속성값(attribute) : 버텍스들
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+           0,                  // 0번째 속성(attribute). 0 이 될 특별한 이유는 없지만, 쉐이더의 레이아웃(layout)와 반드시 맞추어야 합니다.
+           3,                  // 크기(size)
+           GL_FLOAT,           // 타입(type)
+           GL_FALSE,           // 정규화(normalized)?
+           0,                  // 다음 요소 까지 간격(stride)
+           (void*)0            // 배열 버퍼의 오프셋(offset; 옮기는 값)
+        );
+        // 삼각형 그리기!
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 버텍스 0에서 시작해서; 총 3개의 버텍스로 -> 하나의 삼각형
+        glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -72,7 +113,13 @@ int main( void )
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
-	// Close OpenGL window and terminate GLFW
+
+    // Cleanup VBO
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteProgram(programID);
+
+    // Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
 	return 0;
